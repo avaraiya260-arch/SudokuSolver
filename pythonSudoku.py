c19dd437293ba7bd26631cd133e6f4f9c14183a6
avaraiya260-arch <avaraiya260@gmail.com>
2026-05-20 21:48:44 -0400

    Create pythonSudoku.py

---
+++ pythonSudoku.py
@@ -0,0 +1,58 @@
+board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9],
+]
+
+def print_board(board):
+    for i, row in enumerate(board):
+        if i in (3, 6):
+            print("------+-------+------")
+        line = ""
+        for j, n in enumerate(row):
+            if j in (3, 6):
+                line += "| "
+            line += (str(n) if n else ".") + " "
+        print(line)
+
+def is_valid(board, row, col, num):
+    if num in board[row]:
+        return False
+    for r in range(9):
+        if board[r][col] == num:
+            return False
+    br = (row // 3) * 3
+    bc = (col // 3) * 3
+    for r in range(br, br + 3):
+        for c in range(bc, bc + 3):
+            if board[r][c] == num:
+                return False
+    return True
+
+def solve(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+                        if solve(board):
+                            return True
+                        board[row][col] = 0
+                return False
+    return True
+
+print("Puzzle:\n")
+print_board(board)
+
+if solve(board):
+    print("\nSolved:\n")
+    print_board(board)
+else:
+    print("No solution exists.")
